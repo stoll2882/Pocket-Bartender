6f3e4c81b7755607546f3974bb1b473fc51843d1
Sam Toll <sam.x.toll@gmail.com>
2021-12-08 21:29:57 -0800

    updates

--- app/src/main/java/com/toll/sam/pocketbartender/MainActivity.java
+++ app/src/main/java/com/toll/sam/pocketbartender/MainActivity.java
@@ -1,26 +1,49 @@
 package com.toll.sam.pocketbartender;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Fragment;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
     private final String CHANNEL_ID = "abg83jfb";
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    FindDrinksFragment findDrinksFragment = new FindDrinksFragment();
+    BrowseDrinksFragment browseDrinksFragment = new BrowseDrinksFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         createNotificationChannel();
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
         Button notifyButton = findViewById(R.id.notifyButton);
         notifyButton.setOnClickListener(new View.OnClickListener() {
@@ -29,6 +52,69 @@ public class MainActivity extends AppCompatActivity {
                 sendNotification();
             }
         });
+
+        Button getDrinkButton = findViewById(R.id.getDrinkButton);
+        getDrinkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getDrink();
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                return true;
+
+            case R.id.findDrinks:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, findDrinksFragment).commit();
+                return true;
+
+            case R.id.browse:
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, browseDrinksFragment).commit();
+                return true;
+        }
+        return false;
+    }
+
+    // www.thecocktaildb.com/api/json/v1/1/random.php
+
+    private void getDrink() {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Get response string into json format
+                        JSONObject obj = null;
+                        JSONArray arr = null;
+                        try {
+                            arr = new JSONArray(response);
+                            obj = arr.getJSONObject(0);
+                            String date = obj.getString("NeededString");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        System.out.println("Response is: "+ response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("That didn't work!");
+            }
+        });
+
+    // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
     private void sendNotification() {
